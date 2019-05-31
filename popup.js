7ddc667b8d1a2294df1e18b9e2b2d6af8b9ff2ab
jlobasso <jlobasso@gmail.com>
2019-05-31 15:24:01 -0300

    pruebas lenguaje

--- popup.js
+++ popup.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("btn-detect").addEventListener("click", function() {
     var inputText = document.getElementById("text").value;
     var title = chrome.i18n.getMessage("appDescription")
-    //alert(title);
+    alert(title);
     console.log(title)
 
   });
